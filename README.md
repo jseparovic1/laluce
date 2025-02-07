0b23cff4352e3f5b10037526df7a30eaf7f490f7
Jurica Separovic <jurica.separovic@gmail.com>
2025-02-07 22:38:32 +0100

    add landing page

---
+++ README.md
@@ -0,0 +1,17 @@
+# LaLuce
+
+Beauty salon website.
+
+## Development
+```bash
+docker compose up app
+```
+Development server: http://localhost:5173
+
+## Production
+```bash
+docker compose up app-prod
+```
+Production preview: http://localhost:4173
+
+Note: If ports are in use, edit ports in docker-compose.yml 
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true, // automatically open browser
+    host: true  // expose to local network
+  },
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img'
+          }
+          return `assets/${extType}/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
+  css: {
+    devSourcemap: true, // enable CSS source maps
+  },
+  optimizeDeps: {
+    include: [] // add any dependencies that need to be pre-bundled
+  }
+}) 
