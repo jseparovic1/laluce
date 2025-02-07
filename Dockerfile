716d559a81dc74b645f2dd4b5011a665541420c8
Jurica Separovic <jurica.separovic@gmail.com>
2025-02-07 23:07:53 +0100

    improve docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM node:18-alpine as base
 WORKDIR /app
 COPY package*.json ./
-RUN npm ci
 
 # Development stage
 FROM base as dev
-COPY . .
 EXPOSE 3000
 ENV HOST=0.0.0.0
 CMD ["npm", "run", "dev", "--", "--host"]
@@ -13,7 +11,7 @@ CMD ["npm", "run", "dev", "--", "--host"]
 # Production stage
 FROM base as prod
 COPY . .
+RUN npm install
 RUN npm run build
-EXPOSE 3000
 ENV HOST=0.0.0.0
 CMD ["npm", "run", "preview", "--", "--host"] 
--- vite.config.js
+++ vite.config.js
@@ -10,20 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     minify: 'terser',
-    cssMinify: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1]
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img'
-          }
-          return `assets/${extType}/[name]-[hash][extname]`
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
+    cssMinify: true
   },
   css: {
     devSourcemap: true, // enable CSS source maps
